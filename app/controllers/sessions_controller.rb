d8d5b541f37e049608ad0a6414c1772207c4c4ba
Mark Miranda <notmarkmiranda@gmail.com>
2016-05-23 15:51:18 -0600

    closes #1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,9 @@ class SessionsController < ApplicationController
 		end
 		redirect_to root_path
 	end
+
+	def destroy
+		session.delete(:user_id)
+		redirect_to root_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	root 'pages#splash'
 	get '/auth/twitter', as: 'twitter_login'
 	get '/auth/twitter/callback', to: 'sessions#create'
+	get '/logout', to: 'sessions#destroy', as: 'logout'
 end
