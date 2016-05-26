5c38bee012e8eb26faf5ba5a012c3133c509774a
Mark Miranda <notmarkmiranda@gmail.com>
2016-05-25 20:22:31 -0600

    figaro issue (#4)

--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -5,8 +5,8 @@ describe TwitterService do
     VCR.use_cassette("home_timeline") do
       user = User.create(screen_name: "notmarkmiranda",
                          uid: "3300701839",
-                         oauth_token: "3300701839-FYiAhJAUJ7espcn1Ivxnhya3ucplDrQ9A497t5Q",
-                         oauth_token_secret: "N29oGaen6AnqEqZcmP4g8JAqtYN0Z39gcken1cDpAgvXe")
+                         oauth_token: ENV["OAUTH_TOKEN"],
+                         oauth_token_secret: ENV["OAUTH_TOKEN_SECRET"])
       service = TwitterService.new
       tweets = service.home_timeline(user)
       tweet = tweets.first
