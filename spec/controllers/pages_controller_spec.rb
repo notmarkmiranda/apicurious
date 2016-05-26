608da97fc347da0c3a74c2f37402be90d5b1488a
Mark Miranda <notmarkmiranda@gmail.com>
2016-05-25 20:21:58 -0600

    figaro issue

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,8 +10,8 @@ describe PagesController do
     VCR.use_cassette("home_timeline") do
       user = User.create(screen_name: "notmarkmiranda",
                          uid: "3300701839",
-                         oauth_token: "3300701839-FYiAhJAUJ7espcn1Ivxnhya3ucplDrQ9A497t5Q",
-                         oauth_token_secret: "N29oGaen6AnqEqZcmP4g8JAqtYN0Z39gcken1cDpAgvXe")
+                         oauth_token: ENV["OAUTH_TOKEN"],
+                         oauth_token_secret: ENV["OAUTH_TOKEN_SECRET"])
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       get :home
       expect(response).to render_template(:home)
